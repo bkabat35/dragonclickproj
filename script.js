f7639b633f95998f1e147dd51d2059c8cb93649d
bkabat35 <106552698+bkabat35@users.noreply.github.com>
2023-01-02 16:06:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,21 @@ document.getElementById('div').addEventListener('click', e =>{
     e.target.innerText = incr1();
     console.log('dragon-clicker')
 })
+//consider using one closure state for both buttons
+/*class Clicker {
+    #clicks;
+    #node;
+    constructor(node){
+        this.#clicks = 0;
+        this.#node = node;
+        this.#node.addEventListener('click', () => {
+            this.increment();
+        })
+    }
+    increment(){
+        this.#clicks++;
+        this.#node.innerText = this.#clicks;
+    }
+}
+let class1 = new Clicker(classes1, classes2);
+console.log(class1);
